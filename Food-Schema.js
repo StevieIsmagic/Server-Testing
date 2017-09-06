0308d34f2ed6c54552768940d0f5e07a2bb1ec96
Colombianito66 <colombianito66@gmail.com>
2017-09-06 17:09:39 -0400

    complete FoodSchema

---
+++ Food-Schema.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const FoodSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  spicy: {
+    type: Boolean,
+    required: true,
+    unique: true,
+  }
+});
+
+FoodSchema.methods.getName = function() {
+  return this.name;
+};
+
+FoodSchema.statics.getAllFoods = function(cb) {
+  Food.find({}, (err, food) => {
+    if (err) return cb(err);
+    cb(food);
+  });
+};
+
+const Food = mongoose.model('Food', FoodSchema);
+
+module.exports = Food;
